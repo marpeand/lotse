63dae7c77a36e49ad36f83d8659b8a87e977a33d
0x4d5041 <55035865+0x4D5041@users.noreply.github.com>
2023-12-06 18:03:01 -0300

    update blog config

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
     title: "Lotse",
     baseURL: "https://lotse.vercel.app",
+    darkBackground: "#1a1a1a",
 };
 
 module.exports = CONFIG;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import { darkBackground } from "./blog.config";
 
 const config: Config = {
     content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
     theme: {
         extend: {
             colors: {
-                dark: "#1a1a1a",
+                dark: darkBackground || "#1a1a1a",
                 gray: "#656565",
             },
         },

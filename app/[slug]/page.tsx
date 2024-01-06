30e2fd90e8f3740caa60d99afcdf245dbe39c442
marpeand <55035865+marpeand@users.noreply.github.com>
2024-01-06 20:38:21 -0300

    update slugs

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,43 +6,47 @@ import { MdxRenderer } from "../components/Mdx";
 import { format } from "date-fns";
 import CONFIG from "@/blog.config";
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata | undefined> {
-    const post = allPosts.find((post) => post.slug === params.slug);
+export async function generateMetadata({
+  params,
+}): Promise<Metadata | undefined> {
+  const post = allPosts.find((post) => post.slug === params.slug);
 
-    if (!post) {
-        return;
-    }
+  if (!post) {
+    return;
+  }
 
-    const { title, date: publishedTime, slug: slug } = post;
+  const { title, date: publishedTime, slug } = post;
 
-    return {
-        title,
-        openGraph: {
-            title,
-            type: "article",
-            publishedTime,
-            url: `${CONFIG.baseURL}/${slug}`,
-        },
-        twitter: {
-            card: "summary_large_image",
-            title,
-        },
-    };
+  return {
+    title,
+    openGraph: {
+      title,
+      type: "article",
+      publishedTime,
+      url: `${CONFIG.baseURL}/${slug}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+    },
+  };
 }
 export default function Post({ params }) {
-    const post = allPosts.find((post) => post.slug === params.slug);
+  const post = allPosts.find((post) => post.slug === params.slug);
 
-    if (!post) {
-        notFound();
-    }
+  if (!post) {
+    notFound();
+  }
 
-    return (
-        <section>
-            <h1 className="text-2xl font-bold mb-1">{post.title}</h1>
-            <span className="text-gray">{format(new Date(post.date), "dd/MM/yy")}</span>
-            <article className="mt-10 prose prose-invert">
-                <MdxRenderer source={post.body.code} />
-            </article>
-        </section>
-    );
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-1">{post.title}</h1>
+      <span className="text-gray">
+        {format(new Date(post.date), "dd/MM/yy")}
+      </span>
+      <article className="mt-10 prose prose-invert">
+        <MdxRenderer source={post.body.code} />
+      </article>
+    </section>
+  );
 }
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -5,40 +5,43 @@ import { MdxRenderer } from "@/app/components/Mdx";
 import type { Metadata } from "next";
 import CONFIG from "@/blog.config";
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata | undefined> {
-    const project = allProjects.find((post) => post.slug === params.slug);
-    if (!project) {
-        return;
-    }
-    const { title, slug } = project;
+export async function generateMetadata({
+  params,
+}): Promise<Metadata | undefined> {
+  const project = allProjects.find((post) => post.slug === params.slug);
 
-    return {
-        title,
-        openGraph: {
-            title,
-            type: "article",
-            url: `${CONFIG.baseURL}/${slug}`,
-        },
-        twitter: {
-            card: "summary_large_image",
-            title,
-        },
-    };
+  if (!project) {
+    return;
+  }
+  const { title, slug } = project;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      type: "article",
+      url: `${CONFIG.baseURL}/${slug}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+    },
+  };
 }
 
 export default function Post({ params }) {
-    const project = allProjects.find((post) => post.slug === params.slug);
+  const project = allProjects.find((post) => post.slug === params.slug);
 
-    if (!project) {
-        notFound();
-    }
+  if (!project) {
+    notFound();
+  }
 
-    return (
-        <section>
-            <h1 className="text-2xl font-bold">{project.title}</h1>
-            <article className="mt-10 prose prose-invert">
-                <MdxRenderer source={project.body.code} />
-            </article>
-        </section>
-    );
+  return (
+    <section>
+      <h1 className="text-2xl font-bold">{project.title}</h1>
+      <article className="mt-10 prose prose-invert">
+        <MdxRenderer source={project.body.code} />
+      </article>
+    </section>
+  );
 }

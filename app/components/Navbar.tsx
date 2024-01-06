82617b2d2258a9abbd98a1ac7288da3b6174d5f0
marpeand <55035865+marpeand@users.noreply.github.com>
2024-01-06 20:37:42 -0300

    update Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,35 +3,33 @@
 import Link from "next/link";
 
 const navItems = {
-    "/": {
-        name: "home",
-    },
-    "/projects": {
-        name: "projects",
-    },
-    "/about": {
-        name: "about",
-    },
+  "/": {
+    name: "home",
+  },
+  "/projects": {
+    name: "projects",
+  },
+  "/about": {
+    name: "about",
+  },
 };
 
-export function Navbar() {
-    return (
-        <header className="mt-2 mb-6 md:px-0 px-4">
-            <nav className="flex flex-row">
-                <div className="flex flex-row space-x-0 pr-10">
-                    {Object.entries(navItems).map(([path, { name }]) => {
-                        return <NavItem key={path} path={path} name={name} />;
-                    })}
-                </div>
-            </nav>
-        </header>
-    );
+function NavItem({ path, name }: { path: string; name: string }) {
+  return (
+    <Link key={path} href={path} className="flex">
+      <span className="pr-4">{name}</span>
+    </Link>
+  );
 }
 
-function NavItem({ path, name }: { path: string; name: string }) {
-    return (
-        <Link key={path} href={path} className="flex">
-            <span className="pr-4">{name}</span>
-        </Link>
-    );
+export function Navbar() {
+  return (
+    <header className="mt-2 mb-6 md:px-0 px-4">
+      <nav className="flex flex-row pr-10">
+        {Object.entries(navItems).map(([path, { name }]) => {
+          return <NavItem key={path} path={path} name={name} />;
+        })}
+      </nav>
+    </header>
+  );
 }

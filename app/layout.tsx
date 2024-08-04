e2d78a4c985182ea9890debc4093aa2d40bdd2b2
marpeand <55035865+marpeand@users.noreply.github.com>
2024-08-04 17:27:56 -0300

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar } from "./components/Navbar";
-
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="bg-dark text-white max-w-2xl mb-40 flex flex-col mt-4 mx-auto">
+        <Analytics />
         <Navbar />
         <main className="flex-auto flex min-w-0 mt-6 flex flex-col px-4 md:px-0">
           {children}

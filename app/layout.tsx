484842c1e5cf741a9a592835fb583edda0c150ab
marpeand <55035865+marpeand@users.noreply.github.com>
2024-11-23 20:15:34 -0300

    improved SEO Layout and Enhance Meta Tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,48 @@ import { Inter } from "next/font/google";
 import { Navbar } from "./components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+import CONFIG from "@/blog.config";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lotse",
-  description: "Minimalistic dark mode blogging template",
+  title: {
+    default: CONFIG.title,
+    template: `%s | ${CONFIG.title}`,
+  },
+  description: CONFIG.description,
+  metadataBase: new URL(CONFIG.baseURL),
+  openGraph: {
+    title: CONFIG.title,
+    url: CONFIG.baseURL,
+    siteName: `${CONFIG.title}'s website`,
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: `${CONFIG.baseURL}/opengraph-image`,
+        width: 1200,
+        height: 630,
+        alt: `${CONFIG.title}'s website`,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  icons: {
+    shortcut: `${CONFIG.baseURL}/favicon.ico`,
+  },
+  alternates: {
+    types: {
+      "application/rss+xml": `${CONFIG.baseURL}/rss.xml`,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -19,11 +55,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="bg-dark text-white max-w-2xl mb-40 flex flex-col mt-4 mx-auto">
-        <Analytics />
         <Navbar />
-        <main className="flex-auto flex min-w-0 mt-6 flex flex-col px-4 md:px-0">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-4 md:px-0">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
